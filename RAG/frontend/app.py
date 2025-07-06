40aac4e6f2839e5ad1b9b14c52c46cb3f15a0cd8
Alexander <17806067729@163.com>
2025-07-06 16:37:03 +0800

    'debug08'

--- RAG/frontend/app.py
+++ RAG/frontend/app.py
@@ -43,7 +43,7 @@ if st.button('处理PDF并进行RAG流程'):
             client = genai.Client(api_key=api_key)
             pdf_bytes = upload_file.getvalue()
             st.write("The answer is :")
-            pdf_text = main.extract_text()
+            pdf_text = main.extract_text(upload_file)
             pdf_chunks = main.divide_pdf_to_chunks(pdf_text)
             chunks_embedding = main.vector_chunks_embedding(pdf_chunks)
             ans = main.semantic_search(query, pdf_chunks, chunks_embedding)
