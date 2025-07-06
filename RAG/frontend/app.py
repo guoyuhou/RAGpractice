976e6e49f95f511afb0de0a51820db97a134988d
Alexander <17806067729@163.com>
2025-07-06 10:52:21 +0800

    Basic_streamlit_build

--- RAG/frontend/app.py
+++ RAG/frontend/app.py
@@ -1,3 +1,22 @@
 # Strealit app
 import streamlit as st
+
 st.title("RAG")
+st.subheader('刘曜畅2025.7.6')
+
+if "rag_chain" not in st.session_state:
+    st.session_state = None
+
+api_key = st.text_input("请输入你的API_key", type='password')
+upload_file = st.file_uploader('请上传你的PDF文件.', type='pdf')
+
+if st.button('处理PDF并进行RAG流程'):
+    if not api_key:
+        st.error("请输入api密钥")
+    elif not upload_file:
+        st.error("请上传文件")
+    else:
+        st.spinner("Please wait......")
+    
+
+    
--- RAG/simple_rag/main.py
+++ RAG/simple_rag/main.py
@@ -14,3 +14,30 @@ RAD总览
 4. 编码embeddings.
 5. 
 """
+
+# 1. Extract text from pdf
+def extract_text(pdf_path):
+    """
+    Args:
+    pdf_path(str): pdf link
+
+    Returns:
+    str: pdf text
+    """
+    pdf_text = 
+
+
+
+# 2. Divide the text into small chunks
+def divide_pdf_to_chunks(pdf_text):
+    """
+    Args:
+    pdf_text: the text from pdf
+
+    Return:
+    List[str]: Every chunks from pdf
+    """
+    return pdf_chunks
+
+# 3. Load the OpenAI client.
+def 
